31c374efd24e7e2384f03bb4bebb38f6968015ee
Mehdi-17 <mehdi.serra57@gmail.com>
2024-01-14 23:50:10 +0100

    Refactor to add HeroBanner.svelte

---
+++ src/components/HeroBanner.svelte
@@ -0,0 +1,11 @@
+<h1 class="my-4 text-3xl md:text-5xl text-white opacity-75 font-bold leading-tight text-center md:text-left">
+    Bienvenue dans
+    <span class="bg-clip-text text-transparent bg-gradient-to-r from-green-400 via-pink-500 to-purple-500">
+              AyatVenture:
+            </span>
+    Localise le verset !
+</h1>
+<p class="leading-normal text-base md:text-2xl mb-8 text-center md:text-left">
+    L'odyssée qui t'invite à naviguer à travers les trésors du Noble
+    Coran.
+</p>
--- src/routes/Home.svelte
+++ src/routes/Home.svelte
@@ -2,6 +2,7 @@
 import Footer from "../components/Footer.svelte";
 import Nav from "../components/Nav.svelte";
 import SignInForm from "../components/forms/SignInForm.svelte";
+import HeroBanner from "../components/HeroBanner.svelte";
 </script>
 
 <div class="h-full flex flex-col justify-between">
@@ -11,17 +12,7 @@ import SignInForm from "../components/forms/SignInForm.svelte";
     <div class="container mx-auto flex flex-col md:flex-row items-center">
         <!--Left Col-->
         <div class="flex flex-col w-full xl:w-2/5 justify-center lg:items-start overflow-y-hidden">
-            <h1 class="my-4 text-3xl md:text-5xl text-white opacity-75 font-bold leading-tight text-center md:text-left">
-                Bienvenue dans
-                <span class="bg-clip-text text-transparent bg-gradient-to-r from-green-400 via-pink-500 to-purple-500">
-              AyatVenture:
-            </span>
-                Localise le verset !
-            </h1>
-            <p class="leading-normal text-base md:text-2xl mb-8 text-center md:text-left">
-                L'odyssée qui t'invite à naviguer à travers les trésors du Noble
-                Coran.
-            </p>
+           <HeroBanner />
 
             <form class="bg-secondary opacity-75 w-full shadow-lg rounded-lg px-8 pt-6 pb-8 mb-4">
                 <div class="">
