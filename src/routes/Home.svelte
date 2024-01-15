85a0f6af37bc64c79a894917f6504470e18b8a4e
Mehdi-17 <mehdi.serra57@gmail.com>
2024-01-15 14:48:31 +0100

    Remove unnecessary div

--- src/routes/Home.svelte
+++ src/routes/Home.svelte
@@ -1,8 +1,8 @@
 <script lang="ts">
-import Footer from "../components/Footer.svelte";
-import Nav from "../components/Nav.svelte";
-import SignInForm from "../components/forms/SignInForm.svelte";
-import HeroBanner from "../components/HeroBanner.svelte";
+    import Footer from "../components/Footer.svelte";
+    import Nav from "../components/Nav.svelte";
+    import SignInForm from "../components/forms/SignInForm.svelte";
+    import HeroBanner from "../components/HeroBanner.svelte";
 </script>
 
 <div class="h-full flex flex-col justify-between">
@@ -12,17 +12,15 @@ import HeroBanner from "../components/HeroBanner.svelte";
     <div class="container mx-auto flex flex-col md:flex-row items-center">
         <!--Left Col-->
         <div class="flex flex-col w-full xl:w-2/5 justify-center lg:items-start overflow-y-hidden">
-           <HeroBanner />
+            <HeroBanner/>
 
             <form class="bg-secondary opacity-75 w-full shadow-lg rounded-lg px-8 pt-6 pb-8 mb-4">
-                <div class="">
-                    <label class="block text-primary py-2 font-bold mb-2" for="emailaddress">
-                        Connecte-toi ou crée un compte pour jouer
-                    </label>
-<!--TODO : Here display different form depending on if we want to sign up or sign in-->
-                    <SignInForm />
+                <label class="block text-primary py-2 font-bold mb-2" for="emailaddress">
+                    Connecte-toi ou crée un compte pour jouer
+                </label>
+                <!--TODO : Here display different form depending on if we want to sign up or sign in-->
+                <SignInForm/>
 
-                </div>
             </form>
         </div>
 
