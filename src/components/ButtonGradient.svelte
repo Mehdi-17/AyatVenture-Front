a4c66e73165897a6937a77adf5ad1d55ef392998
Mehdi-17 <mehdi.serra57@gmail.com>
2024-04-25 16:21:15 +0200

    Develop quizz screen

--- src/components/ButtonGradient.svelte
+++ src/components/ButtonGradient.svelte
@@ -1,10 +1,12 @@
 <script lang="ts">
     export let disabled: boolean;
     export let text: string;
+
+    export let additionalClass: string;
 </script>
 
 <button on:click
-        class="bg-gradient-to-r from-purple-800 to-green-500 text-white font-bold py-2 px-4 rounded disabled:opacity-65 focus:ring transform transition enabled:hover:scale-105 duration-300 ease-in-out"
+        class="{additionalClass} bg-gradient-to-r from-purple-800 to-green-500 text-white font-bold py-2 px-4 rounded disabled:opacity-65 focus:ring transform transition enabled:hover:scale-105 duration-300 ease-in-out"
         type="button"
         {disabled}
 >
--- src/routes/Quiz.svelte
+++ src/routes/Quiz.svelte
@@ -3,12 +3,15 @@
     import type {Game} from "../components/utils/Game";
     import {GameService} from "../service/GameService";
     import type {Surah} from "../components/utils/Surah";
+    import type {Ayat} from "../components/utils/Ayat";
+    import ButtonGradient from "../components/ButtonGradient.svelte";
 
     const gameService = new GameService();
     const gameSerialized = localStorage.getItem(LOCAL_STORAGE_GAME);
 
     let game: Game;
     let allSurahs: Surah[] = [];
+    let ayatToFind: Ayat;
 
     const getAllSurah = async () => {
         await gameService.getAllSurah().then(surahs => {
@@ -16,24 +19,41 @@
         });
     }
 
-    if (gameSerialized){
-        game =  JSON.parse(gameSerialized);
-        console.log(game);
+    const getAnAyat = async () => {
+        await gameService.getRandomAyat().then(ayat => {
+            ayatToFind = ayat;
+        })
+    }
+
+    if (gameSerialized) {
+        game = JSON.parse(gameSerialized);
 
         //todo: call api to get a question
+        getAnAyat();
         getAllSurah();
     }
 
 
 </script>
 
-<div>
-    <p>Fetch question here</p>
+<div class="flex flex-row items-center h-full justify-center mx-20">
+    <div class="flex flex-col justify-center items-center bg-secondary opacity-75 mx-auto w-full xl:w-4/5 shadow-lg rounded-lg p-5">
+        <h1 class="my-4 text-3xl md:text-5xl text-white opacity-75 font-bold leading-tight text-center md:text-left">
+            Dans quelle sourate se trouve ce verset ?
+        </h1>
+        {#if (ayatToFind)}
+            <p class="text-xl mb-2">{ayatToFind.text_uthmani}</p>
+            <p class="text-xl">{ayatToFind.translations[0].text}</p>
+        {/if}
 
-    {#if (allSurahs.length > 0)}
-        {#each allSurahs as surah}
-            <p>{surah.name_complex}</p>
-        {/each}
-    {/if}
+    <!--TODO: si une réponse est selectionnée, on l'affiche ici    -->
+    </div>
 
+    <div class="flex flex-col items-center bg-secondary opacity-75 h-5/6 overflow-scroll p-2">
+        {#if (allSurahs.length > 0)}
+            {#each allSurahs as surah, index}
+                <ButtonGradient additionalClass="my-2" text={index+1 + ": " +surah.name_complex} disabled={false} />
+            {/each}
+        {/if}
+    </div>
 </div>
