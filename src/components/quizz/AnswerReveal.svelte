edc051dfbbfcf296ab10e815deb15546852ffc71
Mehdi-17 <mehdi.serra57@gmail.com>
2024-05-21 15:01:31 +0200

    Add info to AnswerReveal component

--- src/components/quizz/AnswerReveal.svelte
+++ src/components/quizz/AnswerReveal.svelte
@@ -12,20 +12,20 @@
 <div class="mt-10 w-1/2 flex flex-col items-center">
     <p class="{backGroundAnswer}">{answerIsGood ? "Bonne réponse.".toUpperCase() : "Mauvaise réponse.".toUpperCase()}</p>
 
-    <div class="w-full flex flex-row justify-around">
+    <div class="w-full flex flex-row justify-around my-3">
         {#if playerAnswer !== null}
             <div>
                 <p>Ta réponse:</p>
-                <div>{playerAnswer.translated_name.name}</div>
+                <div>{playerAnswer.name_complex} | {playerAnswer.translated_name.name}</div>
             </div>
         {:else }
             <p>Temps écoulé.</p>
         {/if}
         <div>
             <p>La bonne réponse:</p>
-            <div>{goodAnswer.translated_name.name}</div>
+            <div>{goodAnswer.name_complex} | {goodAnswer.translated_name.name}</div>
         </div>
     </div>
 
-    <p>Tu as gagné : {earnedPoints} point(s).</p>
+    <p class="my-3">Tu as gagné : {earnedPoints} point(s).</p>
 </div>
