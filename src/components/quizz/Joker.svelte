da4d7f9912dd590f696ee8a0c8a0a602ce1e609a
Mehdi-17 <mehdi.serra57@gmail.com>
2024-06-06 13:07:36 +0200

    Add joker icons

--- src/components/quizz/Joker.svelte
+++ src/components/quizz/Joker.svelte
@@ -1,11 +1,16 @@
 <script lang="ts">
 //TODO: afficher les icones et description des Jokers. Au click, on a un joker service qui envoie vers le back le joker sélectionné
-const jokers = ['50/50', 'Verset adjacent', 'Afficher la description'];
+const jokers = [
+    {text: '50/50', src: './src/assets/Jokers/fiftyfifty.png'},
+    {text: 'Verset adjacent', src: 'src/assets/Jokers/adjacentverse.png'},
+    {text: 'Afficher la description', src: 'src/assets/Jokers/description.png'}
+];
 </script>
 
-<div class="flex">
-    J'affiche les Jokers.
+<div class="w-1/3 flex justify-around">
     {#each jokers as joker}
-        <p class="mr-9">{joker}</p>
+        <button>
+            <img src="{joker.src}" alt="{`icon_${joker.text}`}" class="border border-primary rounded p-1">
+        </button>
     {/each}
 </div>
