85512a90b0db1892b4c65e1a615225143b219595
Mehdi-17 <mehdi.serra57@gmail.com>
2024-06-29 14:26:46 +0200

    add Tooltip for joker

---
+++ src/components/Tooltip.svelte
@@ -0,0 +1,20 @@
+<script lang="ts">
+    export let text: string = "";
+    let visible: boolean = false;
+
+    function show() {
+        visible = true;
+    }
+
+    function hide() {
+        visible = false;
+    }
+</script>
+
+<div class="tooltip relative flex flex-col items-center cursor-pointer" on:mouseover={show} on:mouseout={hide}
+     on:focus={show} on:blur={hide} role="tooltip">
+    <slot></slot>
+    <div class="tooltip-text w-[320px] bg-black text-white text-center rounded p-1 absolute bottom-[125%] z-1 transition-opacity duration-300 {visible ? 'visible opacity-1' : 'invisible opacity-0'}">
+        {text}
+    </div>
+</div>
--- src/components/quizz/Joker.svelte
+++ src/components/quizz/Joker.svelte
@@ -1,17 +1,20 @@
 <script lang="ts">
 //TODO: afficher les icones et description des Jokers. Au click, on a un joker service qui envoie vers le back le joker sélectionné
+import Tooltip from "../Tooltip.svelte";
+
 const jokers = [
-    {text: '50/50', src: './src/assets/Jokers/fiftyfifty.png'},
-    {text: 'Verset adjacent', src: 'src/assets/Jokers/adjacentverse.png'},
-    {text: 'Afficher la description', src: 'src/assets/Jokers/description.png'}
+    {text: 'Supprimer 50% des réponses possibles', src: './src/assets/Jokers/fiftyfifty.png'},
+    {text: 'Afficher les versets adjacents', src: 'src/assets/Jokers/adjacentverse.png'},
+    {text: 'Afficher la description de la sourate', src: 'src/assets/Jokers/description.png'}
 ];
 </script>
 
 <div class="w-1/3 flex justify-around">
-<!--    Todo / disabled / enabled joker button and develop mechanics-->
+    <!--    Todo / disabled / enabled joker button and develop mechanics-->
     {#each jokers as joker}
-        <button>
-            <img src="{joker.src}" alt="{`icon_${joker.text}`}" class="border border-primary rounded p-1">
-        </button>
+        <Tooltip text="{joker.text}">
+            <img src="{joker.src}" alt="{`icon_${joker.text}`}"
+                 class="border border-primary rounded p-1">
+        </Tooltip>
     {/each}
 </div>
